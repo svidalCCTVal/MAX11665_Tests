a309bd6464abf20f733dfe8c2216e2ec59be0f20
Sebastian <sebastian.vidal@usm.cl>
2025-05-30 10:04:44 -0400

    Commit de prueba en main

--- MAX11665_Test/Core/Src/main.c
+++ MAX11665_Test/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+//HOLAAAAAAAAAAAAAAAAAAAAAAAAA
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
